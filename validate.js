b9b96d42a0e55c5c009ea3850c7799f6c37c5f2e
Ricardo Rocha <cardrocharj@gmail.com>
2023-03-27 18:27:15 -0300

    commit initial

--- validate.js
+++ validate.js
@@ -1,1 +1,16 @@
-//Seu JavaScript de validação aqui
+const form = document.querySelector(".formcontato__form");
+const inputNome = document.querySelector("#nome");
+const inputEmail = document.querySelector("#email");
+const inputAssunto = document.querySelector("#assunto");
+const textarea = document.querySelector("#mensagem");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  alert("Tudo certo, em breve entrarei em contato, obrigado!");
+
+  inputNome.value = "";
+  inputEmail.value = "";
+  inputAssunto.value = "";
+  textarea.value = "";
+});
